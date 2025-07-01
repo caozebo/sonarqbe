53ff45a3a86621d3c46ec9b79df7fb0a9c3e3e72
caozebo <1663627011@qq.com>
2025-07-01 15:19:13 +0800

    第一次提交

---
+++ src/main/java/com/example/sonarqubegithubdemo/SonarqubeGithubDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonarqubegithubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeGithubDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarqubeGithubDemoApplication.class, args);
+    }
+
+}
